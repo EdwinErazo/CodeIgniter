170abaa3dba192997000950b2ce34aad437e5fa2
Edwin Erazo <32774952+EdwinErazo@users.noreply.github.com>
2019-02-04 17:07:08 -0600

    Initial commit

---
+++ application/controllers/Register.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Register extends CI_Controller {
+
+    public function __construct(){
+
+        parent::__construct();
+       $this->load->library('form_vadilation');
+      $this->load->library('encrypt');
+      $this->load->library('register_model');
+      
+    }
+
+    function index()
+    {
+        $this->load->view('register');
+    }
+
+    function validation(){
+         $this->form_validation->set_rules('user_name', 'Name', 'require|trim');
+         $this->form_validation->set_rules('user_email', 'Email Address', 'require|trim|valid_email|is_unique[codeigniter_register.email]');
+        $this->form_validation->set_rules('user_password','Password', 'required');
+        if($this->form_validation->run()){
+
+            $verification_key = md5(rand());
+            $encrypted_password = $this->encrypt->encode($this->input->post('user_password'));
+            $data =array(
+                'name' => $this->input->post('user_name'),
+                'email' => $this->input->post('user_email'),
+                'password' => $encrypted_password,
+                'verification_key' => $verification_key
+            )
+        }
+        else{
+            $this->index();
+        }
+        }
+}
+
+?>
